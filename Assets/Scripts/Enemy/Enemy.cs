3f1cb66e8a60ef75559c388f7a5f923b8f80fdd6
Educus <kms369400@naver.com>
2025-02-28 22:42:05 +0900

    고블린 작업
    고블린 에셋 추가
    애니메이션 작업
    행동 관련 스크립트 작업
    
    실험실에서만 작동하는 컨트롤러 작업

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,30 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D;
 
 public class Enemy : MonoBehaviour, IHitable
 {
-    [SerializeField] private float hp;
+    [SerializeField] public float hp;
     [SerializeField] private float attackPower;
 
+    private Rigidbody2D rigid;
+    private SpriteRenderer sprite;
+    private Animator anim;
+    public bool isPlaying = true;
 
     private void Awake()
     {
+        rigid = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
         hp = 100;
     }
-    void Start()
-    {
-    }
-
     void Update()
     {
+        if (!isPlaying) return;
+
         if (hp <= 0)
         {
-            gameObject.SetActive(false);
+            Dead();
         }
+
+        anim.SetInteger("Move", (int)rigid.velocity.x);
     }
-    public void Damage(float damage)
+    public void Move(float value)
     {
+        if (!isPlaying) return;
+
+        rigid.velocity = new Vector2(value, 0);
+
+        if (value == 0) return;
+        sprite.flipX = value < 0 ? true : false;
+    }
+    public void Attack()
+    {
+        if (!isPlaying) return;
+
+        anim.SetTrigger("Attack");
+    }
+
+    public void IDamage(float damage)
+    {
+        if (!isPlaying) return;
+
+        anim.SetTrigger("Hit");
         hp -= damage;
+    }
+    public void Dead()
+    {
+        isPlaying = false;
+        anim.SetTrigger("Dead");
+        // gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/Manager/InterfaceManager.cs
+++ Assets/Scripts/Manager/InterfaceManager.cs
@@ -1,6 +1,6 @@
 public interface IHitable   // 피해를 입는 모든 대상
 {
-    public void Damage(float damage);
+    public void IDamage(float damage);
 }
 
 public interface IInteraction   // 상호작용 가능한 모든 대상
