397f33b632a0ebcecf9590c2dfe8526de2598aab
Educus <kms369400@naver.com>
2025-03-08 03:40:43 +0900

    튜토리얼 맵 수정
    임시 포탈 설치(이후 중간 과정 수정 예정)
    포탈 이동에 따른 카메라 범위 변경
    몬스터 설치 및 이동 방식 추가 및 표시(이후 세부 설정 예정)

---
+++ Assets/Scripts/Turotial/TutorialPotal.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialPotal : Interaction
+{
+    [SerializeField] private GameObject outPotal;
+    [SerializeField] private int[] range;
+    [SerializeField] private TutorialCamera camera;
+
+    private void Start()
+    {
+        GetComponent<SpriteRenderer>().sortingOrder = 3;
+    }
+
+    // 튜토리얼 두번째 72,91
+    public override void Interact()
+    {
+        GameManager.Instance.player.transform.position = outPotal.transform.position;
+        GameManager.Instance.startingPoint = outPotal;
+        camera.minMove = new Vector3(range[0], 0, 0);
+        camera.maxMove = new Vector3(range[1], 0, 0);
+    }
+}
