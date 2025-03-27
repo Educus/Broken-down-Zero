a0bbcba19cb12bb0db6c25bf9b8721243b92a2b4
Educus <kms369400@naver.com>
2025-03-27 23:54:14 +0900

    인트로 컷신 이미지 추가

---
+++ Assets/Scripts/Manager/IntroManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroManager : MonoBehaviour
+{
+    [SerializeField] Image intro1;
+    [SerializeField] Image intro2;
+    [SerializeField] Image intro3;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
